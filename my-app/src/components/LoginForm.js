9f29808640d978a845db4b84b712808fe5e3ae52
Nguyễn Viết Quang <nguyenvquang1611@gmail.com>
2022-11-18 11:29:47 +0700

    feat(LoginForm.js, index.css): refactor class name

--- my-app/src/components/LoginForm.js
+++ my-app/src/components/LoginForm.js
@@ -8,7 +8,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 
 const FacebookStyle = { color: '#4267B2' };
 
-function LoginForm({ remember, onSubmit, errorMsg, onSignup, onSocialMediaSignin }) {
+function LoginForm ({ remember, onSubmit, errorMsg, onSignup, onSocialMediaSignin }) {
     const [details, setDetails] = useState({ email: "", password: "", remember: false })
     const submitHandler = e => {
         e.preventDefault();
@@ -20,7 +20,7 @@ function LoginForm({ remember, onSubmit, errorMsg, onSignup, onSocialMediaSignin
             <div className="form-inner">
                 <h2>LOGIN</h2>
                 {(errorMsg !== "") ? (
-                    <div className="error">
+                    <div className="form-error-status">
                         {errorMsg}
                     </div>
                 ) : ""}
@@ -32,26 +32,26 @@ function LoginForm({ remember, onSubmit, errorMsg, onSignup, onSocialMediaSignin
                     <LockOutlined className='form-input-icon' />
                     <input type="password" name='password' id='password' placeholder='Password' onChange={e => setDetails({ ...details, password: e.target.value })} />
                 </div>
-                <div className="form-remember-me">
+                <div className="form-remember-me-group">
                     <input type="checkbox" name="remember-me" id="remember-me" value="remember" onChange={e => setDetails({ ...details, remember: e.target.checked })} />
                     <label htmlFor="checkbox">Remember me</label>
                 </div>
                 <input type="submit" value="LOGIN" />
-                <div className="form-signup-group">
+                <div className="form-other-signin-group">
                     <h6>Or login with</h6>
-                    <div className="form-signup-sites">
-                        <div className="signup-bt" onClick={() => onSocialMediaSignin("Facebook")}>
-                            <FacebookIcon className='signup-icon' fontSize='small' style={FacebookStyle} />
+                    <div className="signup-sites">
+                        <div className="site-bt" onClick={() => onSocialMediaSignin("Facebook")}>
+                            <FacebookIcon className='site-icon' fontSize='small' style={FacebookStyle} />
                             <span style={FacebookStyle}>Facebook</span>
                         </div>
-                        <div className="signup-bt" onClick={() => onSocialMediaSignin("Google")}>
+                        <div className="site-bt" onClick={() => onSocialMediaSignin("Google")}>
                             <img src="https://freesvg.org/img/1534129544.png" alt='google icon' width={15} />
                             Google
                         </div>
                     </div>
-                    <div className="action-signup">
-                        Not a member?&ensp;<u className="link-signup" onClick={onSignup}> Sign up now</u>
-                    </div>
+                </div>
+                <div className="form-signup">
+                    Not a member?&ensp;<u className="link-signup" onClick={onSignup}> Sign up now</u>
                 </div>
             </div>
         </form>
